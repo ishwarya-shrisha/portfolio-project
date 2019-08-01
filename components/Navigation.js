8d421305c4924b3531b0df1e5ba5061a3de9cc9c
IshwaryaR <shrisha.g@gmail.com>
2019-07-31 21:08:59 -0500

    Move Links to be inside of each piece of state

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,25 @@
+function linkBuilder(links){
+  //Recieve an array of 'strings' that are the names of links
+  //return all of <li> items.
+  let listHTML = "";
+  
+  links.forEach(function linkHandler(link) {
+   // console.log(link);
+  listHTML += `<li>${link}</li>`;
+  });
+return listHTML;
+}
 
 export default function(state) {
   return  `
   <nav>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="./About/">About</a></li>
-            <li><a href="./Contact/">Contact</a></li>
+        ${linkBuilder(state.links.primary)}
             <li class="dropdown">
               Portfolio
 
               <ul>
-                <li><a href="">Project1</a></li>
-                <li><a href="">Project2</a></li>
-                <li><a href="">Project3></a></li>
+                ${linkBuilder(state.links.dropdown)}
               </ul>
             </li>
           </ul>
